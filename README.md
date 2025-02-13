114ed30eba7c9e8c189c100c88fdd7b07f48a01b
javadbat <javadbat@gmail.com>
2025-02-13 17:11:41 +0330

    add device detection & silenet event

--- README.md
+++ README.md
@@ -1,6 +1,30 @@
 # jb-core
 
-core modules of jb-design system
+core modules of jb-design system mostly contain functions that help you manage your web-components & tools to connect them to ReactJS components.
+
+## js modules
+
+### listenAndSilentEvent
+
+this function listen to event in the capture phase and stop it's propagation and call your handler so you will be the only one who capture this event used for event forwarding (transformation) in web-components.
+
+```js
+  listenAndSilentEvent(inputDom, 'keyup', yourOnKeyUpHandler);
+  listenAndSilentEvent(inputDom, 'keyup', yourOnKeyUpHandler,{passive:true});
+```
+
+### create events
+
+these functions will create events base on existing event objects used mostly for event forwarding.
+
+```ts
+  #onInputInput(e:InputEvent){
+    const event = createInputEvent('input', e, { cancelable: false });
+    this.dispatchEvent(event);
+  }
+    
+```
+we also have `createKeyboardEvent`, `createInputEvent`, `createFocusEvent`.
 
 ## React modules:
 
--- lib/events.ts
+++ lib/events.ts
@@ -1,32 +1,44 @@
 /**
  * add type to event target for easier access to target
  */
-export type EventTypeWithTarget<TEvent,TTarget> = TEvent & {
+export type EventTypeWithTarget<TEvent, TTarget> = TEvent & {
   target: TTarget;
 };
 /**
+ * this function will stop event from propagate even in capturing phase and will call handler only if event target is the element
+ */
+export function listenAndSilentEvent<TEvent extends Event, TTarget>(element: TTarget, eventName: string, handler: (e:TEvent) => void, options: AddEventListenerOptions = {}) {
+  document.addEventListener(eventName, (e: TEvent) => {
+    if (e.composedPath().includes(element as unknown as EventTarget)) {
+      e.stopPropagation();
+      e.stopImmediatePropagation();
+      handler(e);
+    }
+  },{capture:true,...options});
+}
+/**
  * create a keyboardEvent based on `e` value and replace them with initObj
  */
-export function createKeyboardEvent(eventName:string,e:KeyboardEvent,initObj:KeyboardEventInit){
+export function createKeyboardEvent(eventName: string, e: KeyboardEvent, initObj: KeyboardEventInit) {
   const initialObject: KeyboardEventInit = {
-    altKey:e.altKey,
-    bubbles:e.bubbles,
-    cancelable:e.cancelable,
-    code:e.code,
-    composed:e.composed,
-    ctrlKey:e.ctrlKey,
-    detail:e.detail,
-    isComposing:e.isComposing,
-    key:e.key,
-    location:e.location,
-    metaKey:e.metaKey,
-    repeat:e.repeat,
-    shiftKey:e.shiftKey,
-    view:e.view,
-    charCode:e.charCode,
-    keyCode:e.keyCode,
-    which:e.which,
-    ...initObj   
+    altKey: e.altKey,
+    bubbles: e.bubbles,
+    cancelable: e.cancelable,
+    code: e.code,
+    composed: e.composed,
+    ctrlKey: e.ctrlKey,
+    detail: e.detail,
+    isComposing: e.isComposing,
+    key: e.key,
+    location: e.location,
+    metaKey: e.metaKey,
+    repeat: e.repeat,
+    shiftKey: e.shiftKey,
+    view: e.view,
+    charCode: e.charCode,
+    keyCode: e.keyCode,
+    which: e.which,
+    ...initObj
   };
   const event = new KeyboardEvent(eventName, initialObject);
   return event;
@@ -34,20 +46,20 @@ export function createKeyboardEvent(eventName:string,e:KeyboardEvent,initObj:Key
 /**
  * create a InputEvent based on `e` value and replace them with initObj
  */
-export function createInputEvent(eventName:string,e:InputEvent,initObj:InputEventInit){
+export function createInputEvent(eventName: string, e: InputEvent, initObj: InputEventInit) {
   const initialObject: InputEventInit = {
-    bubbles:e.bubbles,
-    cancelable:e.cancelable,
-    composed:e.composed,
-    detail:e.detail,
-    isComposing:e.isComposing,
-    view:e.view,
-    which:e.which,
-    data:e.data,
-    dataTransfer:e.dataTransfer,
-    inputType:e.inputType,
-    targetRanges: typeof e.getTargetRanges() == "function"?e.getTargetRanges():[],
-    ...initObj   
+    bubbles: e.bubbles,
+    cancelable: e.cancelable,
+    composed: e.composed,
+    detail: e.detail,
+    isComposing: e.isComposing,
+    view: e.view,
+    which: e.which,
+    data: e.data,
+    dataTransfer: e.dataTransfer,
+    inputType: e.inputType,
+    targetRanges: typeof e.getTargetRanges() == "function" ? e.getTargetRanges() : [],
+    ...initObj
   };
   const event = new InputEvent(eventName, initialObject);
   return event;
@@ -56,16 +68,16 @@ export function createInputEvent(eventName:string,e:InputEvent,initObj:InputEven
 /**
  * create a InputEvent based on `e` value and replace them with initObj
  */
-export function createFocusEvent(eventName:string,e:FocusEvent,initObj:FocusEventInit){
+export function createFocusEvent(eventName: string, e: FocusEvent, initObj: FocusEventInit) {
   const initialObject: FocusEventInit = {
-    bubbles:e.bubbles,
-    cancelable:e.cancelable,
-    composed:e.composed,
-    detail:e.detail,
-    view:e.view,
-    which:e.which,
-    relatedTarget:e.relatedTarget,
-    ...initObj   
+    bubbles: e.bubbles,
+    cancelable: e.cancelable,
+    composed: e.composed,
+    detail: e.detail,
+    view: e.view,
+    which: e.which,
+    relatedTarget: e.relatedTarget,
+    ...initObj
   };
   const event = new FocusEvent(eventName, initialObject);
   return event;

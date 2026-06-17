7e095c022e2d71648bd5326690293b51b34fced7
javadbat <javadbat@gmail.com>
2026-06-18 00:14:48 +0330

    feat: add attribute to boolean mapper

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,3 +2,18 @@ export function uniqueId(prefix = '') {
   if (crypto?.randomUUID) return `${prefix}-${crypto.randomUUID()}`;
   return `${prefix}-${Date.now()}-${Math.random().toString(36).slice(2)}`;
 }
+/**
+ * to convert string attribute value to boolean in web-component.
+ */
+export function parseBooleanAttribute(value: string | null, defaultValue = false): boolean {
+  if (value === null) {
+    return defaultValue;
+  }
+  if (value === "" || value.toLowerCase() === "true") {
+    return true;
+  }
+  if (value.toLowerCase() === "false") {
+    return false;
+  }
+  return Boolean(value);
+}
